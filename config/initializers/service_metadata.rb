19ff678ffafe13251d175e5e3ded5980d628cb47
brenetic <brendan.butler@digital.justice.gov.uk>
2020-11-25 00:32:20 +0000

    Base schema validation
    This will validate the base schema only on startup of the app
    
    Co-authored-by: Tomas Destefi <tomas.destefi@digital.justice.gov.uk>
    Co-authored-by: Matt Tei <matt.tei@digital.justice.gov.uk>
    Co-authored-by: Natalie Seeto <natalie.seeto@digital.justice.gov.uk>

--- config/initializers/service_metadata.rb
+++ config/initializers/service_metadata.rb
@@ -5,6 +5,12 @@ if File.exist?(Rails.root.join('spec', 'fixtures', 'service.json'))
   Rails.configuration.service_metadata = JSON.parse(
     File.read(Rails.root.join('spec', 'fixtures', 'service.json'))
   )
+
+  MetadataPresenter::Schema.new(Rails.configuration.service_metadata).validate
+
+  Rails.configuration.service = MetadataPresenter::Service.new(
+    Rails.configuration.service_metadata
+  )
 else
   raise ServiceMetadataNotFoundError.new('No service metadata found')
 end
