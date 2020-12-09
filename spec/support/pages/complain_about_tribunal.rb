46946ef4d1657ffa3d2fcde8ad2187442b2e5b6c
brenetic <brendan.butler@digital.justice.gov.uk>
2020-12-09 18:40:03 +0000

    Add feature specs
    These navigation specs used to live in the MetadataPresenter. We are
    moving them here because the presenter requires a dummy app in order to
    run these type of feature specs. It will become difficult to keep the
    dummy app in sync with the runner going forward.

---
+++ spec/support/pages/complain_about_tribunal.rb
@@ -0,0 +1,8 @@
+class ComplainAboutTribunal < SitePrism::Page
+  set_url '/'
+  element :start_button, :button, 'Start'
+  element :continue_button, :button, 'Continue'
+  element :full_name_field, :field, 'Full name'
+  element :email_field, :field, 'Your email address'
+  element :back_link, :link, 'Back'
+end
