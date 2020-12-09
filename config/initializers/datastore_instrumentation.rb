5d712ef8ee9152f25b0524d2c3c0f40d9c22e36e
Tomas D'Stefano <tomas_stefano@successoft.com>
2020-12-09 13:54:18 -0300

    Add datastore response status to log
    We are logging the datastore request (only the verb and the request
    duration) and we decided to add the response status so it can
    be easier to debug problems in development and also in production
    
    Co-authored-by: Natalie Seeto <natalie.seeto@digital.justice.gov.uk>

--- config/initializers/datastore_instrumentation.rb
+++ config/initializers/datastore_instrumentation.rb
@@ -2,5 +2,6 @@ ActiveSupport::Notifications.subscribe(UserDatastoreAdapter::SUBSCRIPTION) do |n
   url = env[:url]
   http_method = env[:method].to_s.upcase
   duration = ends - starts
-  Rails.logger.info('Datastore request: [%s] %s (%.3f s)' % [url.host, http_method, duration])
+  response_status = env[:status]
+  Rails.logger.info('Datastore request: [%s] %s (%.3f s). Response status: %s' % [url.host, http_method, duration, response_status])
 end
