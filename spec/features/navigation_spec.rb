f92b46c4c510f982aea2d7addf1b68cbdce5ced1
Tomas D'Stefano <tomas_stefano@successoft.com>
2020-12-14 20:04:50 -0300

    Add check your answers and confirmation feature tests
    See https://github.com/ministryofjustice/fb-metadata-presenter/pull/23
    for the implementation

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -5,11 +5,13 @@ RSpec.feature 'Navigation' do
     given_the_service_has_a_metadata
   end
 
-  scenario 'when I navigate through the form' do
+  scenario 'when I navigate forward and back through the form' do
     when_I_visit_the_service
     and_I_add_my_full_name
     and_I_add_my_email
-    and_I_go_back_to_full_name_page
+    and_I_go_back
+    then_I_should_see_my_email
+    and_I_go_back
     then_I_should_see_my_full_name
   end
 
@@ -18,21 +20,56 @@ RSpec.feature 'Navigation' do
     then_I_should_see_not_found_page
   end
 
+  scenario 'when I finish to complete the form' do
+    when_I_visit_the_service
+    and_I_add_my_full_name
+    and_I_add_my_email
+    and_I_add_my_parent_info
+    and_I_check_that_my_answers_are_correct
+    and_I_send_my_application
+    then_I_should_see_the_confirmation_message
+  end
+
   def when_I_visit_a_non_existent_page
     visit '/i-will-initiate-self-destruct'
   end
 
-  def and_I_go_back_to_full_name_page
+  def and_I_go_back
     form.back_link.click
+  end
+
+  def and_I_add_my_parent_info
+    form.parent_field.set('Unknown')
+    form.continue_button.click
+  end
+
+  def and_I_check_that_my_answers_are_correct
+    expect(form.first_name_section.text).to eq('Full name Han Solo')
+    expect(form.email_section.text).to eq('Your email address han.solo@gmail.com')
+    expect(form.parent_section.text).to eq('Parent name Unknown')
+  end
+
+  def and_I_send_my_application
+    form.accept_and_send_button.click
   end
 
   def then_I_should_see_my_full_name
     expect(form.full_name_field.value).to eq('Han Solo')
   end
 
+  def then_I_should_see_my_email
+    expect(form.email_field.value).to eq('han.solo@gmail.com')
+  end
+
   def then_I_should_see_not_found_page
     expect(form.text).to include(
       "The page you were looking for doesn't exist (404)"
     )
+  end
+
+  def then_I_should_see_the_confirmation_message
+    expect(form.confirmation_heading.text).to eq('Complaint sent')
+    expect(form.confirmation_lede.text).to eq('Optional lede')
+    expect(form.confirmation_body.text).to eq("You'll receive a confirmation email")
   end
 end
--- spec/support/pages/complain_about_tribunal.rb
+++ spec/support/pages/complain_about_tribunal.rb
@@ -3,10 +3,16 @@ class ComplainAboutTribunal < SitePrism::Page
   element :start_button, :button, 'Start'
   element :continue_button, :button, 'Continue'
   element :full_name_field, :field, 'Full name'
+  element :parent_field, :field, 'Parent name'
   element :email_field, :field, 'Your email address'
   element :back_link, :link, 'Back'
   elements :error_summary_list, '.govuk-error-summary__list'
   elements :inline_error_messages, '.govuk-error-message'
+  elements :summary_list, '.govuk-summary-list__row'
+  element :accept_and_send_button, :button, 'Accept and send application'
+  element :confirmation_heading, '.govuk-panel__title'
+  element :confirmation_lede, '.govuk-panel__body'
+  element :confirmation_body, '.fb-body'
 
   def error_summary
     error_summary_list.map(&:text)
@@ -19,5 +25,17 @@ class ComplainAboutTribunal < SitePrism::Page
     inline_error_messages.map do |error_message|
       error_message.text.gsub('Error: ', '')
     end
+  end
+
+  def first_name_section
+    summary_list[0]
+  end
+
+  def email_section
+    summary_list[1]
+  end
+
+  def parent_section
+    summary_list[2]
   end
 end
