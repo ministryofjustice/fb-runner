797565766f891f3ff74965bc518d4612d8559880
Natalie Seeto <njseeto@users.noreply.github.com>
2021-02-04 17:41:32 +0000

    Add authentication on start up
    For this to be used, we pass the username and password from the Publisher.
    
    Co-authored-by: Tomas Destefi <tomas.destefi@digital.justice.gov.uk>
    Co-authored-by: Brendan Butler <brendan.butler@digital.justice.gov.uk>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  if ENV['BASIC_AUTH_USER'].present? && ENV['BASIC_AUTH_PASS'].present?
+    http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASS']
+  end
+
   EXCEPTIONS = [
     UserDatastoreAdapter::DatastoreTimeoutError,
     UserDatastoreAdapter::DatastoreClientError
