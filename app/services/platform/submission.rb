5afe8f4bec4646b8a896b880ea1d94b64f22dc4f
Tomas D'Stefano <tomas_stefano@successoft.com>
2021-02-23 16:34:18 -0300

    Do not send the submission if service email output is blank
    Almost the same behaviour as the classic form builder runner

--- app/services/platform/submission.rb
+++ app/services/platform/submission.rb
@@ -3,23 +3,11 @@ module Platform
     include ActiveModel::Model
     attr_accessor :service, :user_data
 
-    REQUIRED_ENV_VARS = %w(
-      SERVICE_EMAIL_PDF_HEADING
-      SERVICE_EMAIL_PDF_SUBHEADING
-      SERVICE_EMAIL_OUTPUT
-      SERVICE_EMAIL_SENDER
-      SERVICE_EMAIL_SUBJECT
-      SERVICE_EMAIL_BODY
-    ).freeze
+    REQUIRED_ENV_VARS = %w(SERVICE_EMAIL_OUTPUT).freeze
 
     validate do
       if REQUIRED_ENV_VARS.any? { |env_var| ENV[env_var].blank? }
-        Rails.logger.info(
-          'Ignoring submission. There are env vars missing: %{env_vars}' % {
-            env_vars: REQUIRED_ENV_VARS.select { |env_var| ENV[env_var].blank? }
-          }
-        )
-        self.errors.add(:base)
+        self.errors.add(:base, "#{REQUIRED_ENV_VARS} env vars are blank.")
       end
     end
 
--- spec/services/platform/submission_spec.rb
+++ spec/services/platform/submission_spec.rb
@@ -29,26 +29,10 @@ RSpec.describe Platform::Submission do
   end
 
   describe '#valid?' do
-    let(:service_email_pdf_heading) { 'Middle earth news' }
-    let(:service_email_pdf_subheading) { 'BREAKING NEWS' }
-    let(:service_email_sender) { 'bilbo@middle-earth.uk' }
-    let(:service_email_subject) { 'Erebor' }
-    let(:service_email_body) { 'Ah, Frodo. Erebor.' }
-
     before do
       allow(ENV).to receive(:[])
-      allow(ENV).to receive(:[]).with('SERVICE_EMAIL_PDF_HEADING')
-        .and_return(service_email_pdf_heading)
-      allow(ENV).to receive(:[]).with('SERVICE_EMAIL_PDF_SUBHEADING')
-        .and_return(service_email_pdf_subheading)
       allow(ENV).to receive(:[]).with('SERVICE_EMAIL_OUTPUT')
         .and_return(service_email_output)
-      allow(ENV).to receive(:[]).with('SERVICE_EMAIL_SENDER')
-        .and_return(service_email_sender)
-      allow(ENV).to receive(:[]).with('SERVICE_EMAIL_SUBJECT')
-        .and_return(service_email_subject)
-      allow(ENV).to receive(:[]).with('SERVICE_EMAIL_BODY')
-        .and_return(service_email_body)
     end
 
     context 'when required env vars are present' do
