38615fbd5f3fa835be0a272e577e93f04eb99056
brenetic <brendan.butler@digital.justice.gov.uk>
2021-06-29 18:28:50 +0100

    Remove sentry performance sampling
    The trail is now over

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,15 +1,6 @@
-EXCLUDE_PATHS = %w[/health /robots.txt].freeze
-
 Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.logger =  Logger.new(STDOUT)
-
-  config.traces_sampler = lambda do |sampling_context|
-    transaction_context = sampling_context[:transaction_context]
-    transaction_name = transaction_context[:name]
-
-    transaction_name.in?(EXCLUDE_PATHS) ? 0.0 : 0.5
-  end
 
   config.before_send = lambda do |event, _hint|
     if event.request && event.request.data
