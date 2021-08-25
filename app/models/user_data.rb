63b39fa36bc7df9090d133ffefa2f5a08a4a1669
Tomas D'Stefano <tomas_stefano@successoft.com>
2021-08-25 08:11:19 -0300

    Add compatibility to avoid erasing user data during the deployment
    Since the session id is being used during the deployment the user id
    will be setting as equal to the session id
    
    For new users will be generated a new token non related to the session
    id

--- app/models/user_data.rb
+++ app/models/user_data.rb
@@ -12,9 +12,7 @@ class UserData
   end
 
   def adapter
-    session[:user_token] ||= SecureRandom.hex(16)
-    session[:user_id] ||= SecureRandom.hex(16)
-
+    create_user_keys
     return @adapter.new(session) if @adapter.present?
 
     if ENV['USER_DATASTORE_URL'].present?
@@ -24,5 +22,15 @@ class UserData
 
       SessionDataAdapter.new(session)
     end
+  end
+
+  def create_user_keys
+    if session[:user_token].present?
+      # backwards compatibility to avoid erasing user data during the deployment
+      session[:user_id] ||= session[:session_id]
+    end
+
+    session[:user_token] ||= SecureRandom.hex(16)
+    session[:user_id] ||= SecureRandom.hex(16)
   end
 end
