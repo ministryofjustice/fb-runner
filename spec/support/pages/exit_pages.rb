b69d9b17cdcc7c093bfb59439fb3acfe522a8b94
Chris Pymm <chris@chrispymm.co.uk>
2022-06-28 09:24:15 +0100

    bump presenter version and update tests

--- spec/support/pages/exit_pages.rb
+++ spec/support/pages/exit_pages.rb
@@ -2,7 +2,7 @@ class ExitPagesFixture < SitePrism::Page
   extend DataContentId
 
   set_url '/'
-  element :start_button, :button, 'Start'
+  element :start_button, :button, I18n.t('actions.start')
   element :page_b_field, :field, 'Page B'
   element :page_c_field, :field, 'Page C'
   element :page_i_field, :field, 'Page I'
@@ -10,7 +10,7 @@ class ExitPagesFixture < SitePrism::Page
   element :page_l_field, :field, 'Page L'
   element :page_knowhere_field, :field, 'Road to knowhere'
   element :page_ghost_field, :field, 'Ghost town'
-  element :continue_button, :button, 'Continue'
+  element :continue_button, :button, I18n.t('actions.continue')
   element :item_3, :radio_button, 'Item 3'
   element :item_2, :radio_button, 'Item 2'
   element :back_link, :link, 'Back'
