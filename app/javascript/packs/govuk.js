1320b11ecd51468557a6f4e6df09d1eed4a68b43
Chris Pymm <chris@chrispymm.co.uk>
2022-07-27 13:52:26 +0100

    Autocomplete component added
    Refactor autocomplete js and remove unnecessary application layout
    
    Keep JS in runner_application for consistency with editor
    
    include autocomplete css
    
    local presenter
    
    tweak

--- app/javascript/packs/govuk.js
+++ app/javascript/packs/govuk.js
@@ -1,6 +1,4 @@
 require("govuk-frontend/govuk/all").initAll()
 require.context('govuk-frontend/govuk/assets/images', true)
-const accessibleAutocomplete = require("accessible-autocomplete")
-window.accessibleAutocomplete = accessibleAutocomplete
 
 import "../styles/govuk.scss"
