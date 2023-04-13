55c1d041131cafd7e69b22198ca8a8c715ac25bd
Natalie Seeto <njseeto@users.noreply.github.com>
2023-04-13 10:14:43 +0100

    appease rubocop

--- app/models/session_data_adapter.rb
+++ app/models/session_data_adapter.rb
@@ -30,10 +30,10 @@ class SessionDataAdapter
 
   def save_progress
     uuid = SecureRandom.uuid
-    OpenStruct.new(uuid: uuid, status: 200)
+    OpenStruct.new(uuid:, status: 200)
   end
 
-  def get_saved_progress(uuid)
+  def get_saved_progress(_uuid)
     OpenStruct.new(status: 200, body: "{\"id\":\"2369f3f3-8bdd-4581-a367-90e34f3aef17\",\"email\":\"email@email.com\",\"secret_question\":\"What was your mother's maiden name?\",\"secret_answer\":\"some more text\",\"page_slug\":\"page-slug\",\"service_slug\":\"some-slug\",\"service_version\":\"27dc30c9-f7b8-4dec-973a-bd153f6797df\",\"user_id\":\"8acfb3db90002a5fc5b43e71638fc709\",\"user_token\":\"b9cca34d4331399c5f461c0ba1c406aa\",\"user_data_payload\":\"{\\\"name_text_1\\\"=\\u003e\\\"Name\\\"}\",\"attempts\":\"0.0\",\"active\":true,\"created_at\":\"2023-04-12T10:28:48.370Z\",\"updated_at\":\"2023-04-12T10:28:48.370Z\"}")
   end
 end
