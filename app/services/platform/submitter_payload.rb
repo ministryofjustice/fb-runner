323c7c652ed233c25a45b9263cd4270c87143aa9
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-05-19 17:54:55 +0100

    Adding default meta information to JSON submitter payload in order for submitter not to reject schema validation

--- app/services/platform/submitter_payload.rb
+++ app/services/platform/submitter_payload.rb
@@ -37,14 +37,20 @@ module Platform
     end
 
     def meta
-      return if ENV['SERVICE_EMAIL_OUTPUT'].blank?
-
-      {
-        pdf_heading: concatenation_with_reference_number(ENV['SERVICE_EMAIL_PDF_HEADING']),
-        pdf_subheading: ENV['SERVICE_EMAIL_PDF_SUBHEADING'].to_s,
-        submission_at: Time.zone.now.iso8601,
-        reference_number: user_data['moj_forms_reference_number']
-      }.compact
+      if ENV['SERVICE_EMAIL_OUTPUT'].blank?
+        {
+          pdf_heading: "JSON Submission for #{service.service_name}",
+          pdf_subheading: '',
+          submission_at: Time.zone.now.iso8601
+        }.compact
+      else
+        {
+          pdf_heading: concatenation_with_reference_number(ENV['SERVICE_EMAIL_PDF_HEADING']),
+          pdf_subheading: ENV['SERVICE_EMAIL_PDF_SUBHEADING'].to_s,
+          submission_at: Time.zone.now.iso8601,
+          reference_number: user_data['moj_forms_reference_number']
+        }.compact
+      end
     end
 
     def actions
