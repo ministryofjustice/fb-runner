acdeeec526d0f70f7eec7532ca12c1bf3520c573
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-05-24 14:13:10 +0100

    Add comment to monkeypatch

--- config/initializers/rails_7_patch.rb
+++ config/initializers/rails_7_patch.rb
@@ -1,3 +1,6 @@
+# This monkeypatch can be removed when the interaction between or x-robots middleware and rails 7 is resolved
+# At the time of upgrade, we end up calling session.enabled? when session is a hash, not an ActionDispatch::Request::Session
+# This lets the middleware continue without throwing
 class Hash
   def enabled?; end
 end
