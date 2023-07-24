c54562a5e7987a62b95f430ad89801c6fb1cadcc
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-07-24 14:26:11 +0100

    Improve focus code

--- app/javascript/src/shared/multiupload.js
+++ app/javascript/src/shared/multiupload.js
@@ -1,16 +1,17 @@
 function showFileUpload() {
   const addAnotherButton  = document.querySelector('[data-multiupload-element="add-another-file"]');
   const uploadFile  = document.querySelector('[data-multiupload-element="upload-another-file"]');
+  const fileInput = document.querySelector('input[type="file"]')
 
   if(!addAnotherButton) return;
   if(!uploadFile) return;
 
-  // addAnotherButton.setAttribute('hidden', '');
   addAnotherButton.style.display = 'none'
   uploadFile.removeAttribute('hidden');
-  uploadFile.focus();
+  if(fileInput) {
+    setTimeout(fileInput.focus(), 0);
+  }
 }
-
 
 // So we can just access required functions from the window object
 window.multiupload = {
