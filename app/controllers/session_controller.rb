7541cdc7b3a805ff456bb335da5048d690daecae
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-08-23 09:12:42 +0100

    Prevent runtime error here

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,6 +1,6 @@
 class SessionController < ApplicationController
   skip_before_action :require_basic_auth
-  skip_before_action VerifySession
+  # skip_before_action VerifySession
 
   def extend
     session[:expire_after] = SESSION_DURATION
