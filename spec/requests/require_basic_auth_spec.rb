cda4752e64554fd5b166bdfabee0209a94560545
Jesus Laiz <zheileman@users.noreply.github.com>
2024-03-12 16:22:37 +0000

    Rebase and update metadata gem

--- spec/requests/require_basic_auth_spec.rb
+++ spec/requests/require_basic_auth_spec.rb
@@ -10,6 +10,36 @@ RSpec.describe 'require basic auth', type: :request do
       get '/'
       expect(response).to redirect_to('/auth')
     end
+
+    context 'show warning depending on environment' do
+      let(:warning_text) { I18n.t('presenter.authorisation.warning') }
+
+      before do
+        allow(ENV).to receive(:[]).with('PLATFORM_ENV').and_return(platform_env)
+        allow(ENV).to receive(:[]).with('DEPLOYMENT_ENV').and_return(deployment_env)
+
+        get '/'
+        follow_redirect!
+      end
+
+      context 'test-production' do
+        let(:platform_env) { 'test' }
+        let(:deployment_env) { 'production' }
+
+        it 'shows the warning' do
+          assert_select 'div.govuk-warning-text', /#{warning_text}/
+        end
+      end
+
+      context 'live-production' do
+        let(:platform_env) { 'live' }
+        let(:deployment_env) { 'production' }
+
+        it 'does not show the warning' do
+          assert_select 'div.govuk-warning-text', count: 0, text: /#{warning_text}/
+        end
+      end
+    end
   end
 
   context 'when session is authorised' do
