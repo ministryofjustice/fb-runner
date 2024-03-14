6cc200f8339dc48868b5c64c42f7e218fb8810ea
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2024-03-14 18:40:47 +0000

    Adding runner tests for regex validator

---
+++ spec/features/regex_spec.rb
@@ -0,0 +1,36 @@
+RSpec.feature 'Navigation' do
+  let(:form) { RegexFixture.new }
+
+  before do
+    allow(VerifySession).to receive(:before).and_return(false)
+  end
+
+  background do
+    when_I_visit_the_service
+  end
+
+  scenario 'happy path' do
+    visit '/multipage'
+    form.capitals_field.set('ABC')
+    form.digits_field.set('123')
+    form.continue_button.click
+    expect(form.text).to include('No number (optional)')
+    form.regex_field.set('abc')
+    form.continue_button.click
+    expect(form.text).to include('Check your answers')
+  end
+
+  scenario 'when required field is blank' do
+    visit '/multipage'
+    form.capitals_field.set('ABC')
+    form.continue_button.click
+    expect(form.error_summary.to_s).to include('Enter an answer')
+  end
+
+  scenario 'when entering wrong regex' do
+    visit '/regex'
+    form.regex_field.set('123')
+    form.continue_button.click
+    expect(form.error_summary.to_s).to include('much match the required format')
+  end
+end
