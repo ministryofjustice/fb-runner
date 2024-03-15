230d1a5a1a8ddde9c09550986205d5b0c222d67e
Jesus Laiz <zheileman@users.noreply.github.com>
2024-03-15 11:45:53 +0100

    Basic auth replacement (#1334)
    * Basic auth replacement PoC
    
    https://trello.com/c/kgK7tVzy
    
    WIP.
    
    * Handle save&return journey
    
    * Cleanup code shifted to presenter
    
    * Update gem sha
    
    * Rebase and update metadata gem
    
    * Update circle config
    
    * Bump mdp sha
    
    * Bump mdp sha
    
    * Bump mdp sha
    
    * Update warning visibility test
    
    * Prepare for release
    
    * Amend wrong version

--- spec/requests/require_basic_auth_spec.rb
+++ spec/requests/require_basic_auth_spec.rb
@@ -6,9 +6,73 @@ RSpec.describe 'require basic auth', type: :request do
       allow(ENV).to receive(:[]).with('BASIC_AUTH_PASS').and_return('somethingboring')
     end
 
-    it 'returns forbidden' do
+    it 'redirects to the auth page' do
       get '/'
-      expect(response.status).to eq(401)
+      expect(response).to redirect_to('/auth')
+    end
+
+    context 'show warning depending on environment' do
+      let(:warning_text) { I18n.t('presenter.authorisation.warning') }
+
+      before do
+        allow(ENV).to receive(:[]).with('PLATFORM_ENV').and_return(platform_env)
+        allow(ENV).to receive(:[]).with('DEPLOYMENT_ENV').and_return(deployment_env)
+
+        get '/'
+        follow_redirect!
+      end
+
+      context 'test-production' do
+        let(:platform_env) { 'test' }
+        let(:deployment_env) { 'production' }
+
+        it 'does not show the warning' do
+          assert_select 'div.govuk-warning-text', count: 0, text: /#{warning_text}/
+        end
+      end
+
+      context 'live-production' do
+        let(:platform_env) { 'live' }
+        let(:deployment_env) { 'production' }
+
+        it 'does not show the warning' do
+          assert_select 'div.govuk-warning-text', count: 0, text: /#{warning_text}/
+        end
+      end
+
+      context 'test-dev' do
+        let(:platform_env) { 'test' }
+        let(:deployment_env) { 'dev' }
+
+        it 'shows the warning' do
+          assert_select 'div.govuk-warning-text', /#{warning_text}/
+        end
+      end
+
+      context 'live-dev' do
+        let(:platform_env) { 'live' }
+        let(:deployment_env) { 'dev' }
+
+        it 'shows the warning' do
+          assert_select 'div.govuk-warning-text', /#{warning_text}/
+        end
+      end
+    end
+  end
+
+  context 'when session is authorised' do
+    before do
+      allow(controller).to receive(:session_authorised?).and_return(true)
+    end
+
+    it 'returns success' do
+      get '/'
+      expect(response.status).to eq(200)
+    end
+
+    it 'skips the auth page' do
+      get '/auth'
+      expect(response).to redirect_to('/')
     end
   end
 
