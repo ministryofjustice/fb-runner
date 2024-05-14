4255ba66183217a5a54cfbf1e19e2e66a262e172
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2024-05-14 14:49:24 +0100

    More linting

--- app/services/platform/microsoft_graph_adapter.rb
+++ app/services/platform/microsoft_graph_adapter.rb
@@ -94,7 +94,7 @@ module Platform
     end
 
     def display_name_for(component, page_label)
-      if component['_type'] == 'radios' || 'checkboxes'
+      if component['_type'] == 'radios' || component['_type'] == 'checkboxes'
         page_label # use the page label or legend if it's a checkbox component
       else
         component['label'] || component['legend'] || '' # autocomplete has a legend not a label
