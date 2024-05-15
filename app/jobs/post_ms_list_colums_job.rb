968e65fbee5a605fd3276dcee3f8cbf2b02f2f3d
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2024-05-15 12:46:11 +0100

    Remove job as it is not used right now and causing crashes

--- app/jobs/post_ms_list_colums_job.rb
+++
@@ -1,8 +0,0 @@
-class PostMSListColumnsJob < ApplicationJob
-  def perform(service)
-    adapter = Platform::MicrosoftGraphAdapter.new
-    adapter.service = service
-
-    adapter.post_list_columns
-  end
-end
