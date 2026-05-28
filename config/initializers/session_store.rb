49639225791f5a9991b1d6477583a0bfe8358169
Ripan <ripan.kumar@justice.gov.uk>
2026-05-28 15:58:12 +0100

    Add secure attribute (#1457)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 Rails.application.config.session_store :cookie_store,
   expire_after: ENV.fetch('SESSION_DURATION', 30).to_i.minutes,
-  key: '_fb_runner_session'
+  key: '_fb_runner_session',
+  secure: Rails.env.production?
