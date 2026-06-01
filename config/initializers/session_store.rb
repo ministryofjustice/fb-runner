0bccdf9ae9d65af1cc32c068a59420c1cbee2926
Ripan <ripan.kumar@justice.gov.uk>
2026-06-01 09:42:24 +0100

    Revert "Add secure attribute (#1457)" (#1458)
    This reverts commit 49639225791f5a9991b1d6477583a0bfe8358169.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,3 @@
 Rails.application.config.session_store :cookie_store,
   expire_after: ENV.fetch('SESSION_DURATION', 30).to_i.minutes,
-  key: '_fb_runner_session',
-  secure: Rails.env.production?
+  key: '_fb_runner_session'
