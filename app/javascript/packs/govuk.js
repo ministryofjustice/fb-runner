2b2655d7755a7272cfca096390990408cd1c9ce9
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2026-08-27 15:53:37 +0100

    6.4

--- app/javascript/packs/govuk.js
+++ app/javascript/packs/govuk.js
@@ -1,7 +1,7 @@
-require("govuk-frontend/govuk/all").initAll()
+require("govuk-frontend").initAll()
 // Ensure GOV.UK Frontend asset directories are picked up by Webpack
 // Use direct filesystem paths to avoid package exports directory resolution issues
-require.context('../../../node_modules/govuk-frontend/govuk/assets/images', true, /\.(png|svg|ico|jpg|jpeg|gif)$/)
-require.context('../../../node_modules/govuk-frontend/govuk/assets/fonts', true, /\.(woff2?|ttf|eot)$/)
+require.context('../../../node_modules/govuk-frontend/dist/govuk/assets/images', true, /\.(png|svg|ico|jpg|jpeg|gif)$/)
+require.context('../../../node_modules/govuk-frontend/dist/govuk/assets/fonts', true, /\.(woff2?|ttf|eot)$/)
 
 import "../styles/govuk.scss"
